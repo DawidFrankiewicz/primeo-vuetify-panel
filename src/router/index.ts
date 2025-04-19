093d2f70c57be0d8210c885bb3c915cc96f8963f
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2025-04-19 18:49:29 +0200

    chore: :fire: Remove unused code

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,20 +7,12 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
-import { routes, handleHotUpdate } from 'vue-router/auto-routes'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: setupLayouts(routes),
 })
-
-// HMR: patch the router at runtime whenever auto‑routes change
-if (import.meta.hot) {
-    // accept updates to the virtual module
-    import.meta.hot.accept()
-    // tell unplugin-vue-router to re-scan and patch this router
-    handleHotUpdate(router)
-}
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
 router.onError((err, to) => {
